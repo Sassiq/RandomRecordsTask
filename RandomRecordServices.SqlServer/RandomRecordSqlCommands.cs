75823fc6f7e86cf5f6dfba3a8a4024f479c8c08d
stanislav.chekhovich@gmail.com <stanislav.chekhovich@gmail.com>
2022-10-25 05:32:38 +0300

    Add RandomRecordSqlCommands class.

---
+++ RandomRecordServices.SqlServer/RandomRecordSqlCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomRecordServices.SqlServer
+{
+    /// <summary>
+    /// Class which use database stored procedures in RecordsDb.
+    /// </summary>
+    public class RandomRecordSqlCommands
+    {
+        private readonly SqlConnection connection;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="RandomRecordSqlCommands"/> class.
+        /// </summary>
+        /// <param name="connection"></param>
+        public RandomRecordSqlCommands(SqlConnection connection)
+        {
+            this.connection = connection;
+        }
+    }
+}
